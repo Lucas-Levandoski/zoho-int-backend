9350e55e97b095fa7cad8851a35ef3324da0d61d
Lucas Levandoski <lucas-levandoski@hotmail.com>
2024-01-09 14:27:45 -0300

    fixing health check async

--- HealthCheck.cs
+++ HealthCheck.cs
@@ -1,12 +1,9 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using ZohoIntegration.TimeLogs.Repositories;
 using System.Web.Http;
 
@@ -21,7 +18,7 @@ public class HealthCheck
     }
 
     [FunctionName("CustomHealthCheck")]
-    public async Task<IActionResult> Run(
+    public IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
         ILogger log)
     {
